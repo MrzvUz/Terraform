e1fe5209270dd98250e2ece80c900608e39f2f96
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-09 23:16:22 +0000

    Separated and dispayed ip and port

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,10 +1,12 @@
-output "IP-Address" {
-  value       = "${docker_container.nodered_container.ip_address}"
+output "IP_Address_And_Port" {
+  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
   description = "The IP address of the container"
 }
 
-output "Container-Name" {
-  value       = "${docker_container.nodered_container.name}"
+output "Container_Name" {
+  value       = docker_container.nodered_container.name
   description = "The Name of the Container"
 }
+
+
 # output "ips" { value = "${join(", ", docker_container.nodered_container.*.ip_address)}" }
