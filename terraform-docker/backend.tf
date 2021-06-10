365ed6848a4b3821d99300b5ab002a8e8eee51be
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-10 18:34:33 +0000

    testing

---
+++ terraform-docker/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformstatebucket-amirzaev"
+    key            = "Terraform/terraform-docker/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraformstate-lock"
+  }
+
+}
