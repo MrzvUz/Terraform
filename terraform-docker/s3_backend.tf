6e0341edc9b46842cd1df53db244d368f2ee19aa
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-10 18:30:40 +0000

    configured and tested credential masking

--- terraform-docker/s3_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraformstatebucket-amirzaev"
-    key            = "Terraform/terraform-docker/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraformstate-lock"
-  }
-
-}
