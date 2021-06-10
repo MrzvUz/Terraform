3b02d5e67d9545542697008016b05c314cca77bc
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-10 01:44:46 +0000

    Worked with terraform main.tf files and output.tf files and also worked with outputting container random names and ports.

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -10,11 +10,22 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+# This resources creates random strings which addes to suffix of our name using python join function.
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  # Join functions is taking random variable from random resource and joins them adds random 4 suffixes to container name.
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    #external = 1880
   }
 }
--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,12 +1,11 @@
-output "IP_Address_And_Port" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
-  description = "The IP address of the container"
-}
-
 output "Container_Name" {
-  value       = docker_container.nodered_container.name
+  # Splat [*].name identifies all resources names and prints them out.
+  value       = docker_container.nodered_container[*].name
   description = "The Name of the Container"
 }
 
-
-# output "ips" { value = "${join(", ", docker_container.nodered_container.*.ip_address)}" }
+output "IP_Address_And_Port" {
+  # join function joines ip and port and prints out on the output dashboard.
+  value       = [for i in docker_container.nodered_container[*] : join(" : ", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP address of the container"
+}
