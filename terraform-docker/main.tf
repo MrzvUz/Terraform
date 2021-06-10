ebc384828dd25e29d5d749c13b2d99fb9e1c4057
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-10 21:38:30 +0000

    Developed nodered_vol, limited container provisioning to a specific number and configured locals.tf file and asked nodred_vol path

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -6,13 +6,19 @@ terraform {
   }
 }
 
+resource "null_resource" "docker_vol" {
+  provisioner "local-exec" {
+    command = "mkdir nodered_vol/ || true && sudo chown -R 1000:1000 nodered_vol/"
+  }
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 # This resources creates random strings which addes to suffix of our name using python join function.
 resource "random_string" "random" {
-  count   = var.container_count
+  count   = local.container_count
   length  = 4
   special = false
   upper   = false
@@ -21,11 +27,15 @@ resource "random_string" "random" {
 
 resource "docker_container" "nodered_container" {
   # Join functions is taking random variable from random resource and joins them adds random 4 suffixes to container name.
-  count = var.container_count
+  count = local.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = var.int_port
-    external = var.ext_port
+    external = var.ext_port[count.index]
+  }
+  volumes {
+    container_path = "/data"
+    host_path      = "${path.cwd}/nodered_vol"
   }
 }
--- terraform-docker/variable.tf
+++ terraform-docker/variable.tf
@@ -1,14 +1,15 @@
 variable "ext_port" {
-  type = number
+  type = list
 
   validation {
-    condition     = var.ext_port <= 65535 && var.ext_port > 0
+    condition     = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
     error_message = "External port must in the valid port range 0 - 65535."
   }
 }
 
 variable "int_port" {
   type = number
+  default = 1880
 
   validation {
     condition     = var.int_port == 1880
@@ -16,7 +17,7 @@ variable "int_port" {
   }
 }
 
-variable "container_count" {
-  type    = number
-  default = 1
-}
+# variable "container_count" {
+#   type    = number
+#   default = 4
+# }
