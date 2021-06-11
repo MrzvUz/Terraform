d0005107f29313df3052647ad28ac5c6d3d81dd4
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-11 22:10:29 +0000

    Developed modules for the terraform docker project

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,19 +1,12 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    }
-  }
-}
-
 resource "null_resource" "docker_vol" {
   provisioner "local-exec" {
     command = "mkdir nodered_vol/ || true && sudo chown -R 1000:1000 nodered_vol/"
   }
 }
 
-resource "docker_image" "nodered_image" {
-  name = var.image[terraform.workspace]
+module "image" {
+  source   = "./image"
+  image_in = var.image[terraform.workspace]
 }
 
 # This resources creates random strings which addes to suffix of our name using python join function.
@@ -28,7 +21,7 @@ resource "docker_container" "nodered_container" {
   # Join functions is taking random variable from random resource and joins them adds random 4 suffixes to container name.
   count = local.container_count
   name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image = docker_image.nodered_image.latest
+  image = module.image.image_out
   ports {
     internal = var.int_port
     external = var.ext_port[terraform.workspace][count.index]
